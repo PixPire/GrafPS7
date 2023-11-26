38c4bbe502fca919434f4fb98eef820e5df05231
Pix <77670@student.pb.edu.pl>
2023-11-26 21:52:33 +0100

    Zaimplementowano rysowanie, edytowanie (przesunięcie o wektor, obrót względem zadanego punktu) zarówno za pomocą myszy jak tekstowo. Możliwość zapisu i odczytu stanu zapisu z pliku, oraz reset rysunku

--- src/PolygonFigure.java
+++ src/PolygonFigure.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class PolygonFigure {
+import java.awt.*;
+import java.io.Console;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class PolygonFigure  implements Serializable {
+
+
+    ArrayList<Point> pointsList=new ArrayList<>();
+
+    public int pointsAmount;
+    public PolygonFigure(){
+
+    }
+
+
+
+    public void setPoints(ArrayList<Point> pointsList){
+        this.pointsList=pointsList;
+    }
+
+    public void addPoint(Point point){
+        pointsList.add(new Point(point.x, point.y));
+
+    }
+
+    public void moveByVector(int vectorX, int vectorY){
+        for (Point point:pointsList
+             ) {   point.x=point.x+vectorX;
+            point.y=point.y+vectorY;
+
+        }
+    }
+
+    public void setPointsAmount(int pointsNumber) {
+        pointsAmount=pointsNumber;
+    }
 }
